52fbd6410f9064f3190a856e61dd919e27c10322
Stephan Griesel <sgriesel@gmail.com>
2022-11-16 17:25:31 +0100

    add comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 const NUM_ROWS = 3;
 const NUM_COLS = 3;
 const NUM_TILES = NUM_ROWS * NUM_COLS;
+// index of the empty tile
 const EMPTY_INDEX = NUM_TILES - 1;
+// defines the minimum and maximum number of random moves to scramble the puzzle board
 const SHUFFLE_MOVES_RANGE = [60, 80];
 const MOVE_DIRECTIONS = ['up', 'down', 'left', 'right'];
 
