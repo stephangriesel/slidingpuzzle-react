64a509b44545aabf4b62339a188a47b70e5d31e6
Stephan Griesel <sgriesel@gmail.com>
2022-11-16 08:40:53 +0100

    vite, tailwind & basic set up

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App grid place-content-center">
+      <div className="grid grid-cols-3 gap-4 place-content-center h-48">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
