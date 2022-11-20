f58e522c2d7e626c82f3c2dfe00f62b9fd95e9ef
Stephan Griesel <sgriesel@gmail.com>
2022-11-20 17:30:56 +0100

    adjust for 9 grid

--- src/App.jsx
+++ src/App.jsx
@@ -184,11 +184,14 @@ function useGameState() {
   return [state.board, state.moves, state.solved, newGame, undo, move];
 }
 
+// Note to self: For more tiles decrease dimension to make them all fit.
+// Currently set for 9 grid, for 16 grid change to 100
+// Remember to also ajust index remainder values
 function Tile ({index, pos, onClick}) {
-  const top = pos[0]*100 + 5;
-  const left = pos[1]*100 + 5;
-  const bgLeft = (index%4)*100 + 5;
-  const bgTop = Math.floor(index/4)*100 + 5;
+  const top = pos[0]*130 + 5;
+  const left = pos[1]*130 + 5;
+  const bgLeft = (index%3)*130 + 5;
+  const bgTop = Math.floor(index/3)*130 + 5;
 
   return <div 
     className='tile'
