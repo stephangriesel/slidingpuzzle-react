714546357c35add02a5ffc60a482b951c42e81a2
Stephan Griesel <sgriesel@gmail.com>
2022-11-20 12:37:42 +0100

    add asset for bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { rand } from './utils/index'
 
 // set grid
@@ -187,9 +187,9 @@ function useGameState() {
 function Tile ({index, pos, onClick}) {
   const top = pos[0]*100 + 5;
   const left = pos[1]*100 + 5;
-  const bgLeft = (index%3)*100 + 5;
-  const bgTop = Math.floor(index/3)*100 + 5;
-  
+  const bgLeft = (index%4)*100 + 5;
+  const bgTop = Math.floor(index/4)*100 + 5;
+
   return <div 
     className='tile'
     onClick={onClick}
