f6b9b0a3af1cd651d99f77e17e8fb71d1f83b0d4
Stephan Griesel <sgriesel@gmail.com>
2022-11-26 10:33:03 +0100

    fix svg classes

--- src/components/icons/Undo.jsx
+++ src/components/icons/Undo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Undo = () => {
     return (
         <div>
-            <svg width="25" height="25" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="79.8 99.7 31.8 99.7 31.8 51.7" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline><path d="M65.8,190.2a88,88,0,1,0,0-124.4l-34,33.9" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path></svg>
+            <svg width="25" height="25" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="79.8 99.7 31.8 99.7 31.8 51.7" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></polyline><path d="M65.8,190.2a88,88,0,1,0,0-124.4l-34,33.9" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></path></svg>
         </div>
     )
 }
