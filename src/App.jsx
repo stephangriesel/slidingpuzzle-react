0c368aad353c4712dcd8a95a6ab55bf5f869c494
Stephan Griesel <sgriesel@gmail.com>
2022-12-06 18:37:43 +0100

    comments update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { rand } from './utils/index'
+import React, { useState } from 'react';
+import { rand } from './utils/index';
 
 import Shuffle from './components/icons/Shuffle'
 import Undo from './components/icons/Undo';
@@ -11,7 +11,7 @@ const NUM_TILES = NUM_ROWS * NUM_COLS;
 // index of the empty tile
 const EMPTY_INDEX = NUM_TILES - 1;
 // defines the minimum and maximum number of random moves to scramble the puzzle board
-const SHUFFLE_MOVES_RANGE = [20, 80]; // TODO: add levels, 0, 10 === easy
+const SHUFFLE_MOVES_RANGE = [0, 80]; // TODO: add levels, 0, 10 === easy
 // direction of cards
 const MOVE_DIRECTIONS = ['up', 'down', 'left', 'right'];
 
@@ -48,7 +48,6 @@ class GameState {
   }
 
   shuffle() {
-    // Shuffling magic happens here
     this.shuffling = true;
     let shuffleMoves = rand(...SHUFFLE_MOVES_RANGE);
     while (shuffleMoves-- > 0) { //  post decrement operator -- followed by the greater than operator > (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Decrement)
@@ -92,6 +91,7 @@ class GameState {
 
     // copy & swop positions
     let boardAfterMove = [...this.board];
+    console.log("board after move", boardAfterMove);
     boardAfterMove[EMPTY_INDEX] = tilePosition;
     boardAfterMove[index] = emptyPosition;
 
@@ -103,7 +103,7 @@ class GameState {
     return true;
   }
 
-  // For loop to check if board is solved
+  // loop to check if board is solved
   isSolved() {
     const ARR_NUM_TILES = Array.from(Array(NUM_TILES).keys()).map(x => x + 1)
     for (let i in ARR_NUM_TILES) {
@@ -117,8 +117,8 @@ class GameState {
   // undo
   undo() {
     if (this.stack.length === 0) return false;
-    this.board = this.stack.pop(); // Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
-    this.moves -= 1; // The subtraction assignment (-=) operator subtracts the value of the right operand from a variable and assigns the result to the variable. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Subtraction_assignment
+    this.board = this.stack.pop(); // pop method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+    this.moves -= 1; // subtraction assignment (-=) operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Subtraction_assignment
   }
 
   moveInDirection(dir) {
@@ -126,7 +126,7 @@ class GameState {
     const epos = this.board[EMPTY_INDEX];
 
     // move tile into place when selected tile moved
-    const posToMove = dir === 'up' ? [epos[0] + 1, epos[1]]
+    const posToMove = dir === 'up' ? [epos[0] + 1, epos[1]] // ternary operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator 
       : dir === 'down' ? [epos[0] - 1, epos[1]]
         : dir === 'left' ? [epos[0], epos[1] + 1]
           : dir === 'right' ? [epos[0], epos[1] - 1]
@@ -134,8 +134,7 @@ class GameState {
 
     // find index tile in posToMove
     let tileToMove = EMPTY_INDEX;
-    console.log("tile to move", tileToMove)
-    const ARR_NUM_TILES = Array.from(Array(NUM_TILES).keys()).map(x => x + 1)
+    const ARR_NUM_TILES = Array.from(Array(NUM_TILES).keys()).map(x => x + 1) // array.from static method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
     console.log("array tiles", ARR_NUM_TILES);
     for (let i in ARR_NUM_TILES) {
       if (this.board[i][0] === posToMove[0] && this.board[i][1] === posToMove[1]) {
@@ -196,13 +195,13 @@ const useGameState = () => {
 // Currently set for 9 grid, for 16 grid change 132 to 100
 // Remember to also ajust index remainder values
 const Tile = ({ index, pos, onClick }) => {
-  const top = pos[0] * 132 + 5;
-  const left = pos[1] * 132 + 5;
-  const bgLeft = (index % 3) * 132 + 5;
-  const bgTop = Math.floor(index / 3) * 132 + 5;
+  const top = pos[0] * 132;
+  const left = pos[1] * 132;
+  const bgLeft = (index % 3) * 132;
+  const bgTop = Math.floor(index / 3) * 132;
 
   return <div
-    className='absolute bg-white/0 bg-tile bg-[length:400px_400px] transition-all duration-300 h-[130px] tile w-[130px]'
+    className='absolute bg-transparent bg-tile bg-[length:400px_400px] transition-all duration-300 h-[130px] tile w-[130px]'
     onClick={onClick}
     style={{ top, left, backgroundPosition: `-${bgLeft}px -${bgTop}px` }} // Reason: Function generates styles, if statically defined things might break
   />;
@@ -219,12 +218,12 @@ const App = () => {
       </div>
       <div className='cursor-move h-[400px] relative w-[400px] scale-75 md:scale-90 lg:scale-100 transition-all'>
         {
-          board.slice(0, -1).map((pos, index) => (
+          board.slice(0, -1).map((pos, index) => ( // The slice() method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
             <Tile key={index} index={index} pos={pos} onClick={move(index)} />
           ))
         }
         {solved &&
-          <div className='absolute bg-confetti flex h-[100%] justify-center opacity-90 w-[100%] z-10'>
+          <div className='absolute bg-confetti flex h-[100%] justify-center opacity-70 w-[100%] z-10'>
               <button className='animate-wiggle text-4xl' onClick={newGame}>
                 NEW GAME?
               </button>
